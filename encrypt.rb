070e45d2d46e586dc68f8f4c2a82ebd3de527fad
gyeon <gyeon19@gmail.com>
2016-07-09 21:28:07 -0400

    everything works yay

--- encrypt.rb
+++ encrypt.rb
@@ -1,5 +1,6 @@
+require 'pry'
 class Nox
-
+  attr_accessor :text, :key 
   HASH = (('A'..'Z').map.with_index.to_a).to_h
 
   def initialize(text, key)
@@ -7,69 +8,65 @@ class Nox
     @key = key.upcase.split("").reject { |e| e.to_s == " " }
   end
 
-  # Assign index to alphabet as a key-value pair
-  # def alphabet_HASH
-  #   HASH = (('A'..'Z').map.with_index.to_a).to_h
-  # end
-
   # Get plaintext / key and delete all spaces
-  # def joined(text)
-  #   text.upcase.split("").reject { |e| e.to_s == " " }
-  # end
 
   def key_cycle(key)
     lengthened_key = []
     key.cycle do |k|
       lengthened_key << k
-      break if lengthened_key.length == key.length
+      break if lengthened_key.length == text.length
     end
-    return lengthened_key
+    lengthened_key
   end
 
   # Convert array of text into array of integers
   def convert_to_ints(text)
-    int_text = text.joined
-    numbers = []
     # iterate through plain text
-    text.select do |letter|
-      # iterate through HASH
-      HASH.each do |key, value|
-        # change each element to value of corresponding key
-        numbers.push(letter = value.to_i) if letter == key
-      end
+    text.map do |letter|
+      HASH[letter]
     end
-    int_text
   end
 
   # Encrypt the combination
-  def encryption_formula(text, key)
-    plain_text = text.convert_to_ints
-    key_text = key.convert_to_ints
+  def encrypt
+    plain_text = convert_to_ints(text)
+    key_text = convert_to_ints(key_cycle(key))
     # Add plaintext and key together
+    binding.pry
     combined = [plain_text, key_text].transpose.map {|num| num.reduce :+}
-    combined.map! {|num| num % 26}
-  end
+    result = combined.map {|num| num % 26}
 
-  def decryption_formula(text, key)
-    plain_text = text.convert_to_ints
-    key_text = key.convert_to_ints
-    # Add plaintext and key together
-    combined = [plain_text, key_text].transpose.map {|num| num.reduce :-}
-    combined.map! {|num| num % 26}
-  end
-
-  def output
-  # Convert back the ints to letters
-    ints = combined
     converted = []
-    ints.each do |num|
+    result.each do |num|
       HASH.each do |key, value|
-        converted.push(num = key) if num == value
+        converted.push(key) if num == value
       end
     end
-    converted
+    puts converted.join("")
+  end
+
+  def decrypt
+   plain_text = convert_to_ints(text)
+    key_text = convert_to_ints(key_cycle(key))
+    # Add plaintext and key together
+    binding.pry
+    combined = [plain_text, key_text].transpose.map {|num| num.reduce :-}
+    result = combined.map {|num| num % 26}
+
+    converted = []
+    result.each do |num|
+      HASH.each do |key, value|
+        converted.push(key) if num == value
+      end
+    end
+    puts converted
   end
 
 end
+
+test = Nox.new("my little pony", "marker")
+test.encrypt
+decrypt_test = Nox.new("YYCSXKXEGYRP", "marker")
+decrypt_test.decrypt
 
 
