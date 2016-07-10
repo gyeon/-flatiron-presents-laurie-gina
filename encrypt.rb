91925c2a003645c4a69db15bbc28ce470d2742ee
gyeon <gyeon19@gmail.com>
2016-07-09 20:06:57 -0400

    edit file according to test.rb

--- encrypt.rb
+++ encrypt.rb
@@ -1,65 +1,65 @@
-# add first plain text integer with first private key integer
-# 3. get private key
-#     a. iterate private key according to the length of plain text and repeat if private key is shorter than plain text
-# 4. encrypt
-#     a. iterate through plain text and private key
-#     b. add first plain text integer with first private key integer
-#     c. mod with 26 so that it wraps if greater than 26
-#     d. then take that integer and convert back to corresponding letter
-#         i. cipher text
-
-# Assign index to alphabet as a key-value pair
-def alphabet_hash
-hash = (('a'..'z').map.with_index.to_a).to_h
-end
-
-# Get plaintext / key and delete all spaces
-def joined(text)
-text.split("").reject { |e| e.to_s == " " }
-end
-
-def iterate_key(key)
-if key.length < text.length
-  (text.length / key.length).times(key)
-  .shift(text.length % key.length)
-end
-
-# Convert array of text into array of integers
-def convert_to_ints(text)
-text.joined
-numbers = []
-# iterate through plain text
-text.select do |letter|
-  # iterate through hash
-  hash.each do |key, value|
-    # change each element to value of corresponding key
-    numbers.push(letter = value.to_i) if letter == key
+class Nox
+  # Assign index to alphabet as a key-value pair
+  def alphabet_hash
+    hash = (('A'..'Z').map.with_index.to_a).to_h
   end
-end
-end
 
-# Encrypt the combination
-def encrypt(text, key)
-plain_text = text.convert_to_ints
-key_text = key.convert_to_ints
-
-# Add plaintext and key together
-added_ints = []
-formula = nil
-plain_text.each do |plain_letter|
-  key_text.each do |key|
-    # Execute algebraic formula
-    formula = (plain_letter + key) % 26
-    added_ints << formula
+  # Get plaintext / key and delete all spaces
+  def joined(text)
+    text.upcase.split("").reject { |e| e.to_s == " " }
   end
-end
 
-# Convert back the ints to letters
-converted = []
-added_ints.each do |num|
-  hash.each do |key, value|
-    converted.push(num = key) if num == value
+  def key_cycle(key)
+    lengthened_key = []
+    key.cycle do |k|
+      lengthened_key << k
+      break if lengthened_key.length == key.length
+    end
+    return lengthened_key
   end
-end
+
+  # Convert array of text into array of integers
+  def convert_to_ints(text)
+    int_text = text.joined
+    numbers = []
+    # iterate through plain text
+    text.select do |letter|
+      # iterate through hash
+      hash.each do |key, value|
+        # change each element to value of corresponding key
+        numbers.push(letter = value.to_i) if letter == key
+      end
+    end
+    int_text
+  end
+
+  # Encrypt the combination
+  def encryption_formula(text, key)
+    plain_text = text.convert_to_ints
+    key_text = key.convert_to_ints
+    # Add plaintext and key together
+    combined = [plain_text, key_text].transpose.map {|num| num.reduce :+}
+    combined.map! {|num| num % 26}
+  end
+
+  def decryption_formula(text, key)
+    plain_text = text.convert_to_ints
+    key_text = key.convert_to_ints
+    # Add plaintext and key together
+    combined = [plain_text, key_text].transpose.map {|num| num.reduce :-}
+    combined.map! {|num| num % 26}
+  end
+
+  def output
+  # Convert back the ints to letters
+    ints = combined
+    converted = []
+    ints.each do |num|
+      hash.each do |key, value|
+        converted.push(num = key) if num == value
+      end
+    end
+    converted
+  end
 
 end
