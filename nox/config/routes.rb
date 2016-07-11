8efe032e337f43571f4a937d30d284892669a6e5
laushinka <laurie.malau@gmail.com>
2016-07-11 01:00:20 -0400

    Add routes

--- nox/config/routes.rb
+++ nox/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'ciphers#root'
-  resources :ciphers
+  get '/ciphers/new_encrypt' => 'ciphers#new'
+  # resources :ciphers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
