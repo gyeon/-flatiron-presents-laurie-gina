c8a8676678fdf9460f37d600eee2a2e83be22cf1
laushinka <laurie.malau@gmail.com>
2016-07-10 22:16:41 -0400

    Refactor codebase, add private methods

--- ascii.rb
+++ ascii.rb
@@ -3,8 +3,8 @@ class Nox
   attr_accessor :text, :key
   # Because ascii corresponds with integers
   # HASH = (('A'..'Z').map.with_index.to_a).to_h
-  START = "A".ord #65
-  FINISH = "Z".ord #90
+   START = "A".ord #65
+   FINISH = "Z".ord #90
 
   def initialize(text, key)
     @text = text.split("").reject { |e| e.to_s == " " }.map(&:ord)
@@ -14,15 +14,6 @@ class Nox
 
   # Get plaintext / key and delete all spaces
 
-  def key_cycle(key)
-    lengthened_key = []
-    key.cycle do |k|
-      lengthened_key << k
-      break if lengthened_key.length == text.length
-    end
-    lengthened_key
-  end
-
 ##### we want ascii characters 32 - 126
   # Encrypt the combination
   def encrypt
@@ -30,10 +21,12 @@ class Nox
     key_text = key_cycle(key)
     # Add plaintext and key together
     combined = [plain_text, key_text].transpose.map {|num| num.inject :+}
-    result = combined.map {|num| num % (FINISH + START) }
-    converted = result.map(&:chr).join("")
-
-    puts converted
+    # The maximum added number in the line above will be 160.
+    # The minimum added number will be 130.
+    # Therefore,
+    # result = combined.map {|num| num % (FINISH + START) }
+    # converted = result.map(&:chr).join("")
+    show_encrypted(combined)
   end
 
   def decrypt
@@ -41,14 +34,26 @@ class Nox
     key_text = key_cycle(key)
     # Add plaintext and key together
     combined = [plain_text, key_text].transpose.map {|num| num.inject :-}
-    result = combined.map {|num| (num % START) + FINISH }
-    converted = result.map(&:chr).join("")
-    puts converted
+    show_encrypted(combined)
   end
 
+  private
+
+  def key_cycle(key)
+    lengthened_key = []
+    key.cycle do |k|
+      lengthened_key << k 
+      break if lengthened_key.length == text.length
+    end
+    lengthened_key
+  end
+
+  def show_encrypted(combined)
+    combined.map {|num| num % (FINISH + START) }.map(&:chr).join("")
+  end
 end
 
-test = Nox.new("i love napping", "booyah")
+test = Nox.new("i love napping three times a day", "booyah")
 test.encrypt
-decrypt_test = Nox.new('0@CT+;(DDG44', 'booyah')
-decrypt_test.decrypt
+decrypt_test = Nox.new("0@CT+;(DDG44;<FC+A0A9Q'1(M", "booyah")
+puts decrypt_test.decrypt
