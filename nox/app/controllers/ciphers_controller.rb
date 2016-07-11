2cc17f8a22bed435b102fec4b3ee7ab5a71d23b5
laushinka <laurie.malau@gmail.com>
2016-07-10 23:44:18 -0400

    Add controller methods

--- nox/app/controllers/ciphers_controller.rb
+++ nox/app/controllers/ciphers_controller.rb
@@ -1,2 +1,18 @@
 class CiphersController < ApplicationController
+  def new
+    @cipher = Cipher.new
+  end
+
+  def create
+    @cipher = Cipher.create(cipher_params)
+    redirect_to cipher_path(@cipher)
+  end
+
+  def show
+    @cipher = Cipher.find(params[:id])
+  end
+
+  def cipher_params
+    params.require(:cipher).permit(:text, :key)
+  end
 end
